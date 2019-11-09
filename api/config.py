b76bbbcef76f9901ab14a588cbfe289ad5bf0ca2
Youmbikuate Lauraine <youmbilauraine@gmail.com>
2019-11-09 19:09:48 +0100

    modification de l'argument echo

--- api/config.py
+++ api/config.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///data.db', echo=False)
+engine = create_engine('sqlite:///data.db', echo=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
