304d4891c5067420a12820893305b0a75304eef8
Youmbikuate Lauraine <youmbilauraine@gmail.com>
2019-11-09 19:12:29 +0100

    imporation du module datetime

---
+++ api/Tables.py
@@ -0,0 +1,28 @@
+from datetime import date, datetime
+
+from sqlalchemy import Column, Integer, String, BOOLEAN, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, exc
+
+from api.config import Base, Session
+
+class Employee(Base):
+    """Employe modele"""
+
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    is_admin = Column(BOOLEAN, nullable=False, default=False)
+    added = Column(DateTime, nullable=False, default=datetime.strptime(str(date.today()), "%Y-%m-%d"))
+    profession = Column(String, nullable=False)
+    type = Column(String)
+    __mapper_args__ = {
+        'polymorphic_identity': 'employee',
+        'polymorphic_on': type
+    }
+
+    def __repr__(self):
+        return "<Employee (name_first='%s' last_name='%s' email='%s') >" % (self.first_name, self.last_name, self.email)
